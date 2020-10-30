4411bdc1038d631621e8b3ec2523122a661a5b5a
Wu Xueheng <xhwu@thoughtworks.com>
2020-10-30 15:51:02 +0800

    feat: remove update requirement for Conjured

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -34,8 +34,6 @@ namespace GildedRose
                     SellIn = 5,
                     Quality = 49,
                 },
-                // this conjured item does not work properly yet
-                new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 },
             };
 
             var app = new GildedRose(items);
