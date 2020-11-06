4805fb53acd048c7b177ed747ceb184180a76669
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-06 23:43:34 +0800

    refactor: move UpdateItem to Class Item to eliminate feature envy, and rename to Update

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -4,9 +4,6 @@ namespace GildedRose
 {
     public class GildedRose
     {
-        private const string AGED_BIRE = "Aged Brie";
-        private const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-        private const string SULFURAS = "Sulfuras, Hand of Ragnaros";
         private IList<Item> items;
         public GildedRose(IList<Item> items)
         {
@@ -18,72 +15,7 @@ namespace GildedRose
             for (var i = 0; i < items.Count; i++)
             {
                 Item item = items[i];
-                UpdateItem(item);
-            }
-        }
-
-        private static void UpdateItem(Item item)
-        {
-            if (item.Name == AGED_BIRE)
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-                }
-
-                item.SellIn = item.SellIn - 1;
-
-                if (item.SellIn < 0 && item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-                }
-            }
-            else if (item.Name == BACKSTAGE_PASSES)
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.SellIn < 11)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-                }
-
-                item.SellIn = item.SellIn - 1;
-
-                if (item.SellIn < 0)
-                {
-                    item.Quality = item.Quality - item.Quality;
-                }
-            }
-            else if (item.Name == SULFURAS)
-            {
-            }
-            else
-            {
-                if (item.Quality > 0)
-                {
-                    item.Quality = item.Quality - 1;
-                }
-
-                item.SellIn = item.SellIn - 1;
-
-                if (item.SellIn < 0 && item.Quality > 0)
-                {
-                    item.Quality = item.Quality - 1;
-                }
+                item.Update();
             }
         }
     }
--- GildedRose/Item.cs
+++ GildedRose/Item.cs
@@ -3,8 +3,77 @@ namespace GildedRose
 {
     public class Item
     {
+        private const string AGED_BIRE = "Aged Brie";
+        private const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+        private const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+
+        public void Update()
+        {
+            if (this.Name == AGED_BIRE)
+            {
+                if (this.Quality < 50)
+                {
+                    this.Quality = this.Quality + 1;
+                }
+
+                this.SellIn = this.SellIn - 1;
+
+                if (this.SellIn < 0 && this.Quality < 50)
+                {
+                    this.Quality = this.Quality + 1;
+                }
+            }
+            else if (this.Name == BACKSTAGE_PASSES)
+            {
+                if (this.Quality < 50)
+                {
+                    this.Quality = this.Quality + 1;
+
+                    if (this.SellIn < 11)
+                    {
+                        if (this.Quality < 50)
+                        {
+                            this.Quality = this.Quality + 1;
+                        }
+                    }
+
+                    if (this.SellIn < 6)
+                    {
+                        if (this.Quality < 50)
+                        {
+                            this.Quality = this.Quality + 1;
+                        }
+                    }
+                }
+
+                this.SellIn = this.SellIn - 1;
+
+                if (this.SellIn < 0)
+                {
+                    this.Quality = this.Quality - this.Quality;
+                }
+            }
+            else if (this.Name == SULFURAS)
+            {
+            }
+            else
+            {
+                if (this.Quality > 0)
+                {
+                    this.Quality = this.Quality - 1;
+                }
+
+                this.SellIn = this.SellIn - 1;
+
+                if (this.SellIn < 0 && this.Quality > 0)
+                {
+                    this.Quality = this.Quality - 1;
+                }
+            }
+        }
     }
 }
