841720337478d74f8235b1be96100112fbfaaa0b
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-06 16:19:20 +0800

    refactor: extract string to const to eliminate magic number

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -4,6 +4,9 @@ namespace GildedRose
 {
     public class GildedRose
     {
+        private const string AGED_BIRE = "Aged Brie";
+        private const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+        private const string SULFURAS = "Sulfuras, Hand of Ragnaros";
         private IList<Item> items;
         public GildedRose(IList<Item> items)
         {
@@ -14,11 +17,11 @@ namespace GildedRose
         {
             for (var i = 0; i < items.Count; i++)
             {
-                if (items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].Name != AGED_BIRE && items[i].Name != BACKSTAGE_PASSES)
                 {
                     if (items[i].Quality > 0)
                     {
-                        if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (items[i].Name != SULFURAS)
                         {
                             items[i].Quality = items[i].Quality - 1;
                         }
@@ -30,7 +33,7 @@ namespace GildedRose
                     {
                         items[i].Quality = items[i].Quality + 1;
 
-                        if (items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (items[i].Name == BACKSTAGE_PASSES)
                         {
                             if (items[i].SellIn < 11)
                             {
@@ -51,20 +54,20 @@ namespace GildedRose
                     }
                 }
 
-                if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (items[i].Name != SULFURAS)
                 {
                     items[i].SellIn = items[i].SellIn - 1;
                 }
 
                 if (items[i].SellIn < 0)
                 {
-                    if (items[i].Name != "Aged Brie")
+                    if (items[i].Name != AGED_BIRE)
                     {
-                        if (items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (items[i].Name != BACKSTAGE_PASSES)
                         {
                             if (items[i].Quality > 0)
                             {
-                                if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (items[i].Name != SULFURAS)
                                 {
                                     items[i].Quality = items[i].Quality - 1;
                                 }
--- GildedRoseTest/GildedRoseTest.cs
+++ GildedRoseTest/GildedRoseTest.cs
@@ -1,6 +1,9 @@
 ﻿using Xunit;
 using System.Collections.Generic;
 using GildedRose;
+using System.Text;
+using System;
+using System.IO;
 
 namespace GildedRoseTest
 {
@@ -13,6 +16,23 @@ namespace GildedRoseTest
             GildedRose.GildedRose app = new GildedRose.GildedRose(items);
             app.UpdateQuality();
             Assert.Equal("foo", items[0].Name);
+        }
+
+        [Fact]
+        public void ThirtyDays()
+        {
+            var fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            var output = fakeoutput.ToString();
+
+            //File.WriteAllText("WriteLines4.txt", output);
+
+            string text = System.IO.File.ReadAllText("WriteLines4.txt");
+
+            Assert.Equal(text, output);
         }
     }
 }
