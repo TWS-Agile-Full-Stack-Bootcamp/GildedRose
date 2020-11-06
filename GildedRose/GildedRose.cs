1211e697e16ef581d9d0499f89c1677c8fbbf1b3
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-06 23:28:27 +0800

    refactor: clean up dead code and extract SULFURAS update logic

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -68,57 +68,31 @@ namespace GildedRose
                     item.Quality = item.Quality - item.Quality;
                 }
             }
+            else if (item.Name == SULFURAS)
+            {
+            }
             else
             {
-                if (item.Name == SULFURAS)
+                if (item.Quality > 0)
                 {
-                    if (item.Quality > 0)
+                    if (true)
                     {
-                        if (false)
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-                    }
-
-                    if (false)
-                    {
-                        item.SellIn = item.SellIn - 1;
-                    }
-
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (false)
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
+                        item.Quality = item.Quality - 1;
                     }
                 }
-                else
+
+                if (true)
+                {
+                    item.SellIn = item.SellIn - 1;
+                }
+
+                if (item.SellIn < 0)
                 {
                     if (item.Quality > 0)
                     {
                         if (true)
                         {
                             item.Quality = item.Quality - 1;
-                        }
-                    }
-
-                    if (true)
-                    {
-                        item.SellIn = item.SellIn - 1;
-                    }
-
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (true)
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
                         }
                     }
                 }
